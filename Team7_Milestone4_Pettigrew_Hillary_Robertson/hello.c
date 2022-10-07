9095cec581bb28752007f855778914790d18a805
AaronPettigrew3 <111756515+AaronPettigrew3@users.noreply.github.com>
2022-10-07 01:17:01 -0500

    format

--- Team7_Milestone4_Pettigrew_Hillary_Robertson/hello.c
+++ Team7_Milestone4_Pettigrew_Hillary_Robertson/hello.c
@@ -1,5 +1,5 @@
 
-#include<math.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
@@ -15,53 +15,42 @@
 #include "driverlib/gpio.h"
 #include "driverlib/adc.h"
 
-
-
-
-
-
-int finddistancefront(){  //PE3
+int findDistanceFront() {  //PE3
     uint32_t ADCValue[1];
     ADCProcessorTrigger(ADC0_BASE, 3);
-                  while(!ADCIntStatus(ADC0_BASE, 3, false))  {}
-                  ADCIntClear(ADC0_BASE, 3);
-                  ADCSequenceDataGet(ADC0_BASE, 3, ADCValue);
-                  int distance= (int)14871*pow(ADCValue[0], -.995); //this eq converts ADC value to cm
-                  return distance;
+    while (!ADCIntStatus(ADC0_BASE, 3, false)) {}
+    ADCIntClear(ADC0_BASE, 3);
+    ADCSequenceDataGet(ADC0_BASE, 3, ADCValue);
+    int distance = (int)14871 * pow(ADCValue[0], -.995); //this eq converts ADC value to cm
+    return distance;
 }
 
-
-int finddistanceside(){   //PE2
+int findDistanceSide() {   //PE2
     uint32_t ADCValue[1];
     ADCProcessorTrigger(ADC0_BASE, 2);
-                     while(!ADCIntStatus(ADC0_BASE, 2, false))  {}
-                     ADCIntClear(ADC0_BASE, 2);
-                     ADCSequenceDataGet(ADC0_BASE, 2, ADCValue);
-                     int distance= (int)14871*pow(ADCValue[0], -.995); //this eq converts ADC value to cm
-                     return distance;
+    while (!ADCIntStatus(ADC0_BASE, 2, false)) {}
+    ADCIntClear(ADC0_BASE, 2);
+    ADCSequenceDataGet(ADC0_BASE, 2, ADCValue);
+    int distance = (int)14871 * pow(ADCValue[0], -.995); //this eq converts ADC value to cm
+    return distance;
 
 }
 
-
-
-void ConfigureADC(){
-
+void ConfigureADC() {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
     GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
 
     ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
-    ADCSequenceStepConfigure(ADC0_BASE,3,0,ADC_CTL_CH0|ADC_CTL_IE|ADC_CTL_END);  //PE3
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE | ADC_CTL_END);  //PE3
     ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
-    ADCSequenceStepConfigure(ADC0_BASE,2,0,ADC_CTL_CH1|ADC_CTL_IE|ADC_CTL_END);  //PE2
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_CH1 | ADC_CTL_IE | ADC_CTL_END);  //PE2
     ADCSequenceEnable(ADC0_BASE, 3);
     ADCSequenceEnable(ADC0_BASE, 2);
-   ADCIntClear(ADC0_BASE, 3);
+    ADCIntClear(ADC0_BASE, 3);
     ADCIntClear(ADC0_BASE, 2);
 }
-
-
 
 void
 ConfigureUART(void)
@@ -94,18 +83,12 @@ ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
-
-
-
-
 int
 main(void)
 {
 
-
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |             //set crystal clock
-                       SYSCTL_OSC_MAIN);
-
+        SYSCTL_OSC_MAIN);
 
 
     ConfigureUART();  // init UART
@@ -113,10 +96,10 @@ main(void)
     ConfigureADC();   // init ADC
 
 
-    while(1){
+    while (1) {
 
-        UARTprintf("distance front[CM] = %d  distance side[CM] = %d      \r", finddistancefront(),finddistanceside());
+        UARTprintf("Distance Front[cm] = %d  Distance Side[cm] = %d      \r", findDistanceFront(), findDistanceSide());
 
-               SysCtlDelay(SysCtlClockGet() / 12);
+        SysCtlDelay(SysCtlClockGet() / 12);
     }
 }
