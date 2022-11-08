bbed8221ad664c87aa1016e9d64934d49659fbdc
Aaron Pettigrew <aaron.pettigrew@hcss.com>
2022-11-07 18:39:54 -0600

    cleanup

--- Team7_Milestone8_Pettigrew_Hillary_Robertson/hello.c
+++ Team7_Milestone8_Pettigrew_Hillary_Robertson/hello.c
@@ -31,13 +31,12 @@ int frontdist;
 int sidedist;
 int pidcounter = 0;
 double LastError = 0;
+
 double findDistanceFront()
 {  //PE3
     uint32_t ADCValue[1];
     ADCProcessorTrigger(ADC0_BASE, 3);
-    while (!ADCIntStatus(ADC0_BASE, 3, false))
-    {
-    }
+    while (!ADCIntStatus(ADC0_BASE, 3, false)) { }
     ADCIntClear(ADC0_BASE, 3);
     ADCSequenceDataGet(ADC0_BASE, 3, ADCValue);
     double distance = 14871 * pow(ADCValue[0], -.995); //this eq converts ADC value to cm
@@ -48,15 +47,13 @@ double findDistanceSide()
 {   //PE2
     uint32_t ADCValue[1];
     ADCProcessorTrigger(ADC0_BASE, 2);
-    while (!ADCIntStatus(ADC0_BASE, 2, false))
-    {
-    }
+    while (!ADCIntStatus(ADC0_BASE, 2, false)) { }
     ADCIntClear(ADC0_BASE, 2);
     ADCSequenceDataGet(ADC0_BASE, 2, ADCValue);
     double distance = 14871 * pow(ADCValue[0], -.995); //this eq converts ADC value to cm
     return distance;
-
 }
+
 void printDistance()
 {
     int i = 0;
@@ -71,13 +68,14 @@ void printDistance()
     UARTprintf("\n");
 
 }
+
 void getstring1(char uartstring[])
 {
-
     uartstring[0] = UARTCharGet(UART1_BASE); //UART0 = USB               UART1 = BLUETOOTH
     uartstring[1] = UARTCharGet(UART1_BASE);
 
 }
+
 void ConfigureADC()
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
@@ -96,6 +94,7 @@ void ConfigureADC()
     ADCIntClear(ADC0_BASE, 3);
     ADCIntClear(ADC0_BASE, 2);
 }
+
 void setCustomSpeedBothMotors()
 {
     char mystring[3];
@@ -105,33 +104,36 @@ void setCustomSpeedBothMotors()
     UARTprintf("both motors set to %d percent\n", percentage);
     leftmotorcustomspeed((double) percentage);
     rightmotorcustomspeed((double) percentage);
-
 }
+
 void leftmotorfast()
 {  // this function sets PWM to 100% duty cycle
     PWM_setDuty(motor1, 65535);
     UARTprintf("Left Motor Fast\n");
 }
+
 void leftmotorslow()
 {  // this function sets PWM to 33% duty cycle
     PWM_setDuty(motor1, 65535 / 3);
     UARTprintf("Left Motor Slow\n");
 }
+
 void leftmotorstop()
 {  // this function turns duty cycle to zero
     PWM_setDuty(motor1, 0);
     //PWM_close(motor1);
     UARTprintf("Left Motor Stop\n");
 }
+
 void leftmotorstart()
 { //enables PWM with a 0% duty cycle. you must call leftmotorfast or leftmotorslow to actually get the motor going.
     motor1 = PWM_open(Board_PWM0, &params);   //PWM0= PWM6 = PF2= Blue LED
     UARTprintf("Left Motor Start \n");
 }
-leftmotorcustomspeed(double speed)
+
+void leftmotorcustomspeed(double speed)
 { // ENTER PERCENTAGE 0-100
     PWM_setDuty(motor1, 65535 * (speed / 100));
-
 }
 
 void rightmotorfast()
@@ -139,30 +141,31 @@ void rightmotorfast()
     PWM_setDuty(motor2, 65535);
     UARTprintf("Right Motor Fast\n");
 }
-rightmotorcustomspeed(double speed)
+
+void rightmotorcustomspeed(double speed)
 { // ENTER PERCENTAGE 0-100
     PWM_setDuty(motor2, 65535 * (speed / 100));
 
 }
+
 void rightmotorslow()
 {  // this function sets PWM to 33% duty cycle
     PWM_setDuty(motor2, 65535 / 3);
     UARTprintf("Right Motor Slow\n");
 }
+
 void rightmotorstop()
 { // duty cycle to zero
     PWM_setDuty(motor2, 0);
     //PWM_close(motor2);
     UARTprintf("Right Motor Stop\n");
 }
+
 void rightmotorstart()
 { //enables PWM with a 0% duty cycle. you must call rightmotorfast or rightmotorslow to actually get the motor going.
     motor2 = PWM_open(Board_PWM1, &params);   //PWM0= PWM7 = PF3= GREEN LED
     UARTprintf("Right Motor Start\n");
 }
-
-
-
 
 struct lut
 {
@@ -170,17 +173,21 @@ struct lut
     void (*func)(void);
 };
 
-struct lut LUT[25] = {  { "bc", setCustomSpeedBothMotors },
-                       { "ls", leftmotorstart }, { "lf", leftmotorfast }, {
-                               "sl", leftmotorslow },
-                       { "el", leftmotorstop }, { "rs", rightmotorstart }, {
-                               "rf", rightmotorfast },
-                       { "sr", rightmotorslow }, { "er", rightmotorstop }, {
-                               "pd", printDistance } };
+struct lut LUT[25] = {
+{ "bc", setCustomSpeedBothMotors },
+{ "ls", leftmotorstart },
+{ "lf", leftmotorfast },
+{ "sl", leftmotorslow },
+{ "el", leftmotorstop },
+{ "rs", rightmotorstart },
+{ "rf", rightmotorfast },
+{ "sr", rightmotorslow },
+{ "er", rightmotorstop },
+{ "pd", printDistance }};
 
 void bluetoothSendMessage(char *array);
 
-ConfigureUART(void)
+void ConfigureUART(void)
 {
     //
     // Enable the GPIO Peripheral used by the UART.
@@ -215,10 +222,8 @@ void btUART()
     char uartstring[3] = "";
     int i = 0;
     void (*fun)(void);
-
     while (1)
     {
-
         getstring1(uartstring); //pulls 2 character string from UART
         UARTprintf("your input: %s\n", uartstring);
         for (i = 0; i < 25; i++)
@@ -229,15 +234,13 @@ void btUART()
                 fun();
                 break;
             }
-
         }
         if (i == 25) UARTprintf("Invalid input!\n");
-
     }
 }
+
 void pid()
 {
-
     //PID CALCULATIONS
     double frontdistance = findDistanceFront();
     double sidedistance = findDistanceSide();
@@ -246,7 +249,8 @@ void pid()
     double I = 1.5 * (Error + LastError);
     double D = 1 * (Error - LastError);
     double PID = (P + I + D);
-    if (PID > 98) PID = 98; // dont let PID value go over 98
+    if (PID > 98)
+        PID = 98; // dont let PID value go over 98
 
     if (Error > 0)
     {                       //ROBOT IS VEERING LEFT        SLOW DOWN RIGHT MOTOR
@@ -254,20 +258,21 @@ void pid()
         leftmotorcustomspeed(99);
     }
     if (Error < 0)
-    {                         // ROBOT IS VEERING RIGHT       SLOW DOWN LEFT MOTOR
+    {                       // ROBOT IS VEERING RIGHT       SLOW DOWN LEFT MOTOR
         leftmotorcustomspeed(99 - PID);
         rightmotorcustomspeed(99);
     }
 
     LastError = Error;
 
-    if (frontdistance < 8)//IF FRONTDISTANCE <8 cm U TURN
+    if (frontdistance < 8)                       //IF FRONTDISTANCE <8 cm U TURN
     {
 
         GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, GPIO_PIN_7); //SET LEFT MOTOR TO REVERSE
         leftmotorcustomspeed(99);
         rightmotorcustomspeed(99);
-        while (findDistanceFront() < 16); //turn until front distance >16
+        while (findDistanceFront() < 16)
+            ; //turn until front distance >16
         GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, 0); //set motor back to forward
     }
     TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
@@ -320,14 +325,10 @@ int reflectance()
         return 0;      //UARTprintf("White\n");
 }
 
-
-
-int numThinLines=0;
-
+int numThinLines = 0;
 
 void blacklineinterrupt()  //CALLED EVERY 10 MS
 {
-
     int cycles = 0;
     while (reflectance() == 1)       //WHILE SENSOR READING BLACK
     {
@@ -336,21 +337,18 @@ void blacklineinterrupt()  //CALLED EVERY 10 MS
     if (cycles > 15)            //IF MORE THAN 15 CYCLES IT MUST BE THICCCCCC
     {
         // STOP
-        UARTprintf("Large strip hit numCycles:%d\n",cycles);
+        UARTprintf("Large strip hit numCycles:%d\n", cycles);
     }
-    if((cycles>3)&&(cycles<16)){                     //IF LESS THAN 16 cycles must be thin line
+    if ((cycles > 3) && (cycles < 16))
+    {                     //IF LESS THAN 16 cycles must be thin line
         numThinLines++;
-        UARTprintf("Thin line #%d hit   numCycles:%d \n",numThinLines,cycles);
+        UARTprintf("Thin line #%d hit   numCycles:%d \n", numThinLines, cycles);
     }
-
     TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 }
 
-
-
 int main(void)
 {
-
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
     Board_initGeneral();
     Board_initGPIO();
@@ -367,11 +365,6 @@ int main(void)
     rightmotorstart();
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_7);
-
-    /*while(1){
-     reflectance();
-     SysCtlDelay(SysCtlClockGet() / 6);
-     }*/
 
     BIOS_start();
 
